0b98dc879cba129b46fa55bd2af668e28709c230
shehzad <shehzadamin697@gmail.com>
2025-11-05 18:53:25 +0500

    save

--- src/app/employee-registration/registration/registration.component.ts
+++ src/app/employee-registration/registration/registration.component.ts
@@ -28,6 +28,7 @@ import { CommonService } from 'src/app/shared/services/common.service';
 import { ConfigService } from 'src/app/shared/services/config.service';
 import { v4 as uuidv4 } from 'uuid';
 import { JobService } from 'src/app/domain/services/job.service ';
+import { forkJoin } from 'rxjs';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -101,7 +102,7 @@ export class RegistrationComponent implements OnInit {
       EmployeeDesignationId: ['',Validators.required],
       EmploymentTypeId: ['',Validators.required],
       MaritalStatusId: [''],
-      JobDescriptionId: ['',Validators.required],
+      JobDescriptionId: [''],
       EducationTypeId: [''],
       EmployeeTypeId: [''],
       StreetAddress: [''],
@@ -147,14 +148,11 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit(): void {
     this.employeeId = this.route.snapshot.queryParamMap.get('id');
-    if (this.employeeId != null && this.employeeId != '') {
-      this.isEdit = true;
-      this.getEmployeeById(this.employeeId);
-    }else{
-      this.isEdit=false;
-    }
-    if(!this.isEdit)
+
+    if(!this.employeeId){
     this.getEmployeeCode();
+    }
+
      this.getGenderByLovCode();
      this.getgroups();
 
@@ -166,13 +164,44 @@ export class RegistrationComponent implements OnInit {
      this.getEmployeeTypeByLovCode();
      this.getEmploymentTypeByLovCode();
      this.getjobDescriptions();
+
+         if (this.employeeId != null && this.employeeId != '') {
+      this.isEdit = true;
+      this.getEmployeeById(this.employeeId);
+    }else{
+      this.isEdit=false;
+    }
   }
   onGroupChange(event:any){
     this.getdepartmentsbyGroupId(event.id);
     this.getlevelsbyGroupCode(event.code)
     this.getfunctionsbyGroupId(event.id);
-
   }
+
+  onGroupChanging(event: any,item:any) {
+  const departments$ = this.departmentService.getdepartmentsbyGroupId(event.id);
+  const levels$ = this.levelService.getlevelsbyGroupCode(event.code);
+  const functions$ = this.functionService.getfunctionsbyGroupId(event.id);
+
+  forkJoin({
+    departments: departments$,
+    levels: levels$,
+    functions: functions$
+  }).subscribe({
+    next: (result) => {
+      // Assign data from all three API calls
+      this.departmentList = result.departments.data;
+      this.levelList = result.levels.data;
+      this.functionList = result.functions.data;
+            this.registrationForm.controls['LevelId'].setValue(item.levelId);
+            this.registrationForm.controls['DepartmentId'].setValue(item.departmentId);
+            this.registrationForm.controls['FunctionId'].setValue(item.functionId);
+    },
+    error: (err) => {
+      this.toast.error(err.message);
+    }
+  });
+}
   getEmployeeById(employeeId: string) {
     this.employeeService.getEmployeeById(employeeId).subscribe({
       next: (result) => {
@@ -209,7 +238,7 @@ export class RegistrationComponent implements OnInit {
 
       this.levelService.getlevelsbyGroupCode(groupCode).subscribe({
         next: result => {
-          debugger;
+
           this.levelList=[];
           this.levelList = result.data;
         },
@@ -276,21 +305,25 @@ export class RegistrationComponent implements OnInit {
         year: Number(splitDOBJoining[0]),
       };
     }
-    debugger;
+
     this.registrationForm.controls['Cnic'].setValue(item.cnic);
     this.registrationForm.controls['Contact'].setValue(item.contact);
     this.registrationForm.controls['GenderId'].setValue(item.genderId);
-    this.registrationForm.controls['DepartmentId'].setValue(item.departmentId);
-    this.registrationForm.controls['FunctionId'].setValue(item.functionId);
 
     this.registrationForm.controls['GroupId'].setValue(item.groupId);
+    if(item.groupId){
+
+      let selectedGroup = this.groupList.find(group => group.id === item.groupId);
+       this.onGroupChanging(selectedGroup,item);
+
+    }
     this.registrationForm.controls['EmployeeDesignationId'].setValue(item.employeeDesignationId);
     this.registrationForm.controls['EmployeeTypeId'].setValue(item.employeeTypeId);
     this.registrationForm.controls['MaritalStatusId'].setValue(item.maritalStatusId);
     this.registrationForm.controls['EducationTypeId'].setValue(item.educationTypeId);
    this.registrationForm.controls['JobDescriptionId'].setValue(item.jobDescriptionId);
    this.registrationForm.controls['EmploymentTypeId'].setValue(item.employmentTypeId);
-   this.registrationForm.controls['LevelId'].setValue(item.levelId);
+
 
     this.registrationForm.controls['IsPicturePermission'].setValue(item.isPicturePermission);
     this.registrationForm.controls['Picture'].setValue(item.picture);
@@ -334,7 +367,7 @@ export class RegistrationComponent implements OnInit {
 
     this.levelService.getall().subscribe({
       next: result => {
-        debugger;
+
         this.levelList=[];
         this.levelList = result.data;
       },
@@ -355,7 +388,7 @@ export class RegistrationComponent implements OnInit {
 
     this.jobService.getall().subscribe({
       next: result => {
-        debugger;
+
         this.jobDescriptionList=[];
         this.jobDescriptionList = result.data;
       },
@@ -423,27 +456,37 @@ export class RegistrationComponent implements OnInit {
   }
   formSubmit() {
     this.submitted = true;
-    debugger;
+
     if (!this.registrationForm.valid) {
       this.toast.error('Please complete all required fields before proceeding.');
       return;
     }
-    let DateOfBirth =
-      this.dateofbirthPicker.year +
-      '-' +
-      this.dateofbirthPicker.month +
-      '-' +
-      this.dateofbirthPicker.day;
+    // let DateOfBirth =
+    //   this.dateofbirthPicker.year +
+    //   '-' +
+    //   this.dateofbirthPicker.month +
+    //   '-' +
+    //   this.dateofbirthPicker.day;
+      let yeard = this.dateofbirthPicker.year;
+let monthd = String(this.dateofbirthPicker.month).padStart(2, '0');
+let dayd = String(this.dateofbirthPicker.day).padStart(2, '0');
+
+let DateOfBirth = `${yeard}-${monthd}-${dayd}`;
     // if(this.RenewPicker!=null || this.RenewPicker!=undefined){
     //   let RenewPicker = this.RenewPicker.year + '-' + this.RenewPicker.month + '-' + this.RenewPicker.day;
     //   this.registrationForm.value['RenewDate'] = RenewPicker;
     // }
-    let DateOfJoining =
-      this.dateOfJoiningPicker.year +
-      '-' +
-      this.dateOfJoiningPicker.month +
-      '-' +
-      this.dateOfJoiningPicker.day;
+    // let DateOfJoining =
+    //   this.dateOfJoiningPicker.year +
+    //   '-' +
+    //   this.dateOfJoiningPicker.month +
+    //   '-' +
+    //   this.dateOfJoiningPicker.day;
+      let year = this.dateOfJoiningPicker.year;
+let month = String(this.dateOfJoiningPicker.month).padStart(2, '0');
+let day = String(this.dateOfJoiningPicker.day).padStart(2, '0');
+
+let DateOfJoining = `${year}-${month}-${day}`;
     this.registrationForm.value['DateOfBirth'] = DateOfBirth;
 
     this.registrationForm.value['DateOfJoining'] = DateOfJoining;
